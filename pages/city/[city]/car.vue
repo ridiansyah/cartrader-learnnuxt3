3452f7a8dce654814641f4cfb990cbf0be615540
Wahyu Ridiansyah <wahyuridiansyah@gmail.com>
2023-01-13 15:34:14 +0700

    implement usehead

--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,7 +1,9 @@
 <script setup>
 const route = useRoute();
 useHead({
-  title: toTitleCase(route.params.name),
+  title: `${
+    route.params.make ? toTitleCase(route.params.make) : "Cars"
+  } in ${toTitleCase(route.params.city)}`,
 });
 function toTitleCase(str) {
   return str.replace(/\w\S*/g, function (txt) {
@@ -9,16 +11,17 @@ function toTitleCase(str) {
   });
 }
 </script>
+
 <template>
   <div>
     <NavBar />
     <div
       class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
     >
-      <CarDetailHero />
-      <CarDetailAttributes />
-      <CarDetailDescription />
-      <CarDetailContact />
+      <div class="mt-32 flex">
+        <CarSideBar />
+        <NuxtPage />
+      </div>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
+<script setup>
+useHead({
+  title: "CarTrader",
+});
+</script>
+
 <template>
   <div>
     <NavBar />
