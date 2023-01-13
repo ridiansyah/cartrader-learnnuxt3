13a50cd4ed0234912b03fce9a0d17c3e1ef197ce
Wahyu Ridiansyah <wahyuridiansyah@gmail.com>
2023-01-13 15:45:51 +0700

    vetur

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -12,7 +12,18 @@ function toTitleCase(str) {
 <template>
   <div>
     <div
-      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+      class="
+        mx-auto
+        mt-4
+        max-w-7xl
+        space-y-4
+        px-4
+        xs:px-8
+        sm:px-10
+        lg:px-16
+        pb-16
+        w-3/5
+      "
     >
       <CarDetailHero />
       <CarDetailAttributes />
