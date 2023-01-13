94b897dd1d4f2bb3ca251c028efe66b8916decc0
Wahyu Ridiansyah <wahyuridiansyah@gmail.com>
2023-01-13 13:48:27 +0700

    navbar

--- app.vue
+++ app.vue
@@ -7,48 +7,5 @@
       <a class="text-3xl font-mono" href="">cartrader</a>
     </header>
     <!-- Navbar -->
-
-    <!-- Cars Page -->
-    <div
-      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
-    >
-      <div class="mt-32 flex">
-        <!-- Car Side Bar -->
-        <div class="shadow border w-64 mr-10 z-30 h-[190px]">
-          <div
-            class="p-5 flex justify-between relative cursor-pointer border-b"
-          >
-            <h3>Location</h3>
-            <h3 class="text-blue-500 capitalize">Sleman</h3>
-            <!-- <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-              <input type="text" class="border p-1 rounded" />
-              <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">
-                Apply
-              </button>
-            </div> -->
-          </div>
-
-          <div
-            class="p-5 flex justify-between relative cursor-pointer border-b"
-          >
-            <h3>Make</h3>
-            <h3 class="text-blue-500 capitalize">Honda</h3>
-          </div>
-
-          <div
-            class="p-5 flex justify-between relative cursor-pointer border-b"
-          >
-            <h3>Price</h3>
-            <h3 class="text-blue-500 capitalize">Any</h3>
-          </div>
-        </div>
-        <!-- Car Side Bar -->
-
-        <!-- Card Cards -->
-
-        <!-- Card Cards -->
-      </div>
-    </div>
-    <!-- Cars Page -->
   </div>
 </template>
