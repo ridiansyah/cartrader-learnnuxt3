bc25933afccb42b4160e54237fd842ef7a06749a
Wahyu Ridiansyah <wahyuridiansyah@gmail.com>
2023-01-13 13:02:16 +0700

    feat: Building the Search Bar

--- app.vue
+++ app.vue
@@ -14,6 +14,18 @@
       <div class="z-10 absolute w-full h-full flex justify-center items-center">
         <div class="text-center w-1/2">
           <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+          <!-- Home Search Bar -->
+          <div
+            class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto"
+          >
+            <input
+              type="text"
+              class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
+              placeholder="Search by City..."
+            />
+            <button class="bg-sky-500 px-10 text-white">Search</button>
+          </div>
+          <!-- Home Search Bar -->
         </div>
       </div>
       <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
