60827b1c8676724fc1bee82628c2f93de599fcd4
Wahyu Ridiansyah <wahyuridiansyah@gmail.com>
2023-01-13 16:21:00 +0700

    dynamic car render

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,21 +1,24 @@
+<script setup>
+const props = defineProps({
+  car: Object,
+});
+</script>
+
 <template>
   <div
     class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+    @click="navigateTo(`/car/${car?.name}-${car?.id}`)"
   >
     <div class="flex h-full">
-      <img
-        src="https://img.cintamobil.com/2021/10/21/cWC5a43e/brio-1ebd.png"
-        alt=""
-        class="w-[300px] h-full"
-      />
+      <img :src="car?.url" alt="" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
-          <h1 class="text-2xl text-blue-700">Honda Brio RS</h1>
+          <h1 class="text-2xl text-blue-700">{{ car?.name }}</h1>
           <p class="text-grey-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            {{ car?.description }}
           </p>
         </div>
-        <h1 class="mt-auto text-xl">IDR 221.000.000</h1>
+        <h1 class="mt-auto text-xl">${{ car?.price }}</h1>
       </div>
     </div>
   </div>
