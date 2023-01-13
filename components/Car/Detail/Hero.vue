fa92dea1d25b494c4265b747dcc2459a7fa38046
Wahyu Ridiansyah <wahyuridiansyah@gmail.com>
2023-01-13 16:41:24 +0700

    namenya ketinggalan lol

--- components/Car/Detail/Hero.vue
+++ components/Car/Detail/Hero.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 <template>
   <div class="mt-10">
     <img class="w-full" :src="car?.url" alt="" />
-    <h1 class="mt-10 text-4xl">Honda Brio RS</h1>
+    <h1 class="mt-10 text-4xl">{{ car?.name }}</h1>
     <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
       <div class="flex">
         <p class="mr-2">{{ car?.seats }} Seats</p>
